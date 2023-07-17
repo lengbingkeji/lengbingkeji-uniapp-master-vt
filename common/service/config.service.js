5ba17f8b9e6a1d41ef13ce6bae2f57c4aefb37d8
lengbingkeji <1517699467@qq.com>
2023-07-17 09:56:20 +0800

    冷冰科技-uniApp端项目初始化...

---
+++ common/service/config.service.js
@@ -0,0 +1,18 @@
+let BASE_URL = ''
+
+
+if (process.env.NODE_ENV == 'development') {
+    //BASE_URL = 'http://127.0.0.1:8080/jeecg-boot' // 开发环境
+	BASE_URL = 'http://127.0.0.1:8080/lengbingkeji-boot-vt' // 开发环境
+} else {
+	//BASE_URL = 'http://boot.jeecg.org:8080/jeecg-boot' // 生产环境
+	BASE_URL = 'https://www.lengbingkeji.com/lengbingkeji-boot-vt/' // 生产环境
+}
+let staticDomainURL = BASE_URL+ '/sys/common/static';
+
+const configService = {
+	apiUrl: BASE_URL,
+	staticDomainURL: staticDomainURL
+};
+
+export default configService
